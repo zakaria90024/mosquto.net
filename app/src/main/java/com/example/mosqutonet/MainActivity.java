37e51c8ee88ccf8e4f49cc3245245f9d0e0942e9
Md. Zakaria Ahnaf <zakaria112203@gmail.com>
2024-05-05 10:39:06 +0600

    Mosquito app

---
+++ app/src/main/java/com/example/mosqutonet/MainActivity.java
@@ -0,0 +1,77 @@
+package com.example.mosqutonet;
+
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.os.Bundle;
+import android.view.View;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.Button;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button buttonDown, buttonUp;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        buttonDown = findViewById(R.id.buttonDown);
+        buttonUp = findViewById(R.id.buttonUp);
+
+
+        buttonDown.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                ConnectivityManager mgr = (ConnectivityManager) getApplication().getSystemService(Context.CONNECTIVITY_SERVICE);
+                NetworkInfo netInfo = mgr.getActiveNetworkInfo();
+
+                if (netInfo != null) {
+                    if (netInfo.isConnected()) {
+                        WebView myWebView = (WebView) findViewById(R.id.webview);
+                        myWebView.loadUrl("http://192.168.4.1/L");
+                        myWebView.setWebViewClient(new WebViewClient());
+                    } else {
+                        Toast.makeText(MainActivity.this, "Not Connected Internet", Toast.LENGTH_SHORT).show();
+                    }
+                } else {
+                    Toast.makeText(MainActivity.this, "Not Connected Internet", Toast.LENGTH_SHORT).show();
+                }
+
+
+            }
+        });
+
+
+        buttonUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ConnectivityManager mgr = (ConnectivityManager) getApplication().getSystemService(Context.CONNECTIVITY_SERVICE);
+                NetworkInfo netInfo = mgr.getActiveNetworkInfo();
+
+                if (netInfo != null) {
+                    if (netInfo.isConnected()) {
+                        WebView myWebView = (WebView) findViewById(R.id.webview);
+                        myWebView.loadUrl("http://192.168.4.1/H");
+                        myWebView.setWebViewClient(new WebViewClient());
+                    } else {
+                        Toast.makeText(MainActivity.this, "Not Connected Internet", Toast.LENGTH_SHORT).show();
+                    }
+                } else {
+                    Toast.makeText(MainActivity.this, "Not Connected Internet", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+
+    }
+}
